66cd9c5eaad0e16021e8046216c83a9568a8a629
CliffordF <yourpalcliffy@gmail.com>
2018-11-11 14:17:49 +1100

    updated exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { EventAppComponent } from './event-app.component';
+import { EventsListComponent } from './events/events.list.component';
+import { EventThumbnailComponent } from './events/events-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/events.service';
+import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
+import { RouterModule } from '@angular/router';
+import { CreateEventComponent } from './events/create-event/create-event.component';
+import { NotFoundComponent } from './errors/notfound/notfound.component';
+import { EventRouterActivator } from './events/shared/event-router-activator.service';
+import { EventListResolverService } from './events/shared/event-list-resolver.service'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    EventAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    NotFoundComponent
+  ],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouterActivator,
+    EventListResolverService,
+    {
+      provide: 'canDeactivateCreateEvent', useValue: checkDirtyState
+    }
+  ],
+  bootstrap: [EventAppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
+export function checkDirtyState(component: CreateEventComponent) {
+   if (component.isDirty) {
+      return window.confirm('Navigating away will result in changes being lost.!');
+   }
+   return true;
+}
