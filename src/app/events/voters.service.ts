eeabf1f173408a386e77f6eb7569f313e854ed68
CliffordF <yourpalcliffy@gmail.com>
2018-11-17 07:20:55 +1100

    unit tests

--- src/app/events/voters.service.ts
+++ src/app/events/voters.service.ts
@@ -12,7 +12,7 @@ export class VotersService {
   constructor(private _http: HttpClient) { }
 
   deleteVoter(eventId: number, session: ISession, voterName: string) {
-    // session.voters = session.voters.filter(voter => voter !== voterName);
+    session.voters = session.voters.filter(voter => voter !== voterName);
     const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
     this._http.delete(url)
       .pipe(catchError(this.handleError('deleteVoter')))
@@ -20,7 +20,7 @@ export class VotersService {
   }
 
   addVoter(eventId: number, session: ISession, voterName: string) {
-    // session.voters.push(voterName);
+    session.voters.push(voterName);
     const options = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
     const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
     this._http.post(url, {}, options)
